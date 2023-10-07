80bfffa1c7e8080562c112aafd0ae827b496ee7b
Artur Zagitov <146620636+arturzagitov@users.noreply.github.com>
2023-10-07 11:36:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,15 +12,15 @@ st.info("Сайт компании: https://taxcom.ru/", icon="📃")
          
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {"role": "assistant", "content": "Спроси меня про машиночитаемые доверенности (МЧД)"}
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Загружается документация. Пожалуйста, ожидайте 1-2 минуты."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="Ты эксперт по документации на тему машиночитаемые доверенности (МЧД). Отвечай на все вопросы, используя документацию "Машиночитаемая доверенность". Старайтесь, чтобы твои ответы были техническими и основывались на фактах – не придумывай. Отвечай на языке загруженного документа"))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
